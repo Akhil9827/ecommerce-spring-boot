65f0243da22f14de6e0879818478aa6a5498fe76
Akhilesh samal <akhileshasamal@gmail.com>
2026-02-02 10:58:08 +0530

    Added GlobalExceptionHandler to handle exception globally

--- ecom-sb/src/main/java/com/ecommerce/project/service/CategoryService.java
+++ ecom-sb/src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -4,7 +4,7 @@ import com.ecommerce.project.model.Category;
 
 import java.util.List;
 
-public interface CategoryService {
+public interface  CategoryService {
     List<Category> getAllCategories();
     void createCategory(Category category);
 
