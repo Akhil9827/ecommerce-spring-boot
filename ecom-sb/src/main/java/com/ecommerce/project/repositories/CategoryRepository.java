c3526122eee23b2361a1868938232b2d8934ebf1
Akhilesh samal <akhileshasamal@gmail.com>
2026-02-11 11:31:41 +0530

    Implemented another Get Mapping to get products by CategoryId

--- ecom-sb/src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
+++ ecom-sb/src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category,Long> {
+public interface CategoryRepository extends JpaRepository<Category,Long> {////here we passed (Category,Long) means here Category is Entity type and Long is type of id
+
     Category findByCategoryName(String categoryName);
 }
